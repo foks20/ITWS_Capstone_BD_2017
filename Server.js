151ea4bc625e86032694211cdaadf9ee0e88eb06
Joseph Noel <jtnoel96@gmail.com>
2017-11-11 21:08:42 -0500

    Server now connects to MongoDB, events on event.html are now pulled from database.

--- Server.js
+++ Server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var path = __dirname + '/views/';
 
+// Routing
+
 router.use(function (req, res, next) {
-	console.log('/' + req.method);
+	// console.log('/' + req.method);
 	next();
 });
 
@@ -35,10 +39,58 @@ router.get('/itlp.html', function(req, res){
 app.use('/', router);
 
 app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/controllers'));
 
 // app.use('*', function(req, res) {
 // 	res.sendFile(path + '404.html');
 // });
+
+// Connect to MongoDB
+
+var dburl = 'mongodb://localhost:27017/bd'
+MongoClient.connect(dburl, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected to MongoDB");
+    // Clear events and insert new ones - for testing purposes
+    db.collection('events').drop()
+    insertEvents(db, function() {});
+    
+    router.get('/events', function(req, res) {
+    findEvents(db, function(docs) {
+        res.send(docs);
+        console.log('GET: events');
+    });
+});
+});
+
+// Events
+
+var insertEvents = function(db, callback) {
+    var events = db.collection('events');
+    events.insertMany([
+        {name: "Philanthropy Hosting Event", details: "It's a philanthropy thingy.", contactName: "Jane Smith", contactEmail: "smithj@bd.com", points: "300", committee: "Philanthropy"},
+        {name: "Second Round Interview Participation", details: "Recruitment is life.", contactName: "Jane Smith", contactEmail: "smithj@bd.com", points: "250", committee: "Recruiting"},
+        {name: "Time Sheet Compliance", details: "For the non-existant hourly ITLPs.", contactName: "Jane Smith", contactEmail: "smithj@bd.com", points: "100", committee: "Initiatives/Training"},
+        {name: "Annual Meeting Planning", details: "Everyone loves meetings about meetings.", contactName: "Jane Smith", contactEmail: "smithj@bd.com", points: "200", committee: "Marketing"}
+    ], function(err, result) {
+        assert.equal(null, err);
+        assert.equal(4, result.result.n);
+        assert.equal(4, result.ops.length);
+        console.log("Inserted 4 events");
+        callback(result);
+    });
+}
+
+var findEvents = function(db, callback) {
+    var events = db.collection('events');
+    events.find({}).toArray(function(err, docs) {
+        assert.equal(null, err);
+        assert.equal(4, docs.length);
+        console.log("Retrieved Events");
+        callback(docs);
+    });
+}
+
 
 app.listen(3000, function() {
 	console.log('Live at port 3000');
---
+++ controllers/eventController.js
@@ -0,0 +1,9 @@
+var app = angular.module("bd", []);
+app.controller("eventCtrl", function($scope, $http) {
+    // Gets all events from server/database
+    $http.get("events").success(function(data, status, headers, config) {
+        $scope.events = data;
+    }).error(function(data, status, headers, config) {
+        console.log("Error retrieving events.");
+    });
+});
